73a319a40aa9161b8563c05ba75755ce83f50d30
d3224421-sys <d3224421@gmail.com>
2026-06-06 01:15:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.secret_key = generate_secret_key()
 
 # ipapi API endpoint
 IP_GEOLOCATION_API = 'http://api.ipapi.com/'
-API_KEY = "-"  # Replace with your actual API key
+API_KEY = "838e93dfc274ca61974539864e29388e"  # Replace with your actual API key
 
 # with image
 # NOTE: you could remove this part of code if you don't want it.
